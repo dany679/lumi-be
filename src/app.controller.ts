972f689d99b05da0474288bc5be6aaa761e798c6
Daniellymoreira <daniellymoreira30@gmail.com>
2024-03-01 12:24:27 -0300

    fix/hello-public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './utils/decorator/public.decorator';
 import { GetCurrentUserById } from './utils/guard/get-user-by-id.decorator';
 import { JwtAuthGuard } from './utils/guard/jwt-auth.guard';
 
-@Controller()
+@Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @UseGuards(JwtAuthGuard)
   @Get()
   getHello(@GetCurrentUserById() id: number): string {
+    console.log(id);
+    return this.appService.getHello();
+  }
+  @Public()
+  @Get()
+  getHelloAuth(@GetCurrentUserById() id: number): string {
     console.log(id);
     return this.appService.getHello();
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Public } from './utils/decorator/public.decorator';
 
 @Injectable()
 export class AppService {
-  @Public()
   getHello(): string {
     return 'Hello World! not-authenticated';
+  }
+  getHelloAuth(): string {
+    return 'Hello World! authenticated';
   }
 }
